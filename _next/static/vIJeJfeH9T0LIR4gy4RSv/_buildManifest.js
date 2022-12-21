96ab5e8c669c4bb9779fd7e030938257aa511578
univerxegame <114832062+univerxegame@users.noreply.github.com>
2022-12-22 03:42:53 +0530

    Add files via upload

---
+++ _next/static/vIJeJfeH9T0LIR4gy4RSv/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/9734fa8bdaa944af.css","static/chunks/pages/index-eeb77ad014202c95.js"],"/_error":["static/chunks/pages/_error-effe22be6ff34abe.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
